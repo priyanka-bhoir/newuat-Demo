07c40838d2e83dcf5abe1823e28a4ad7c4337f1f
Priyanka <bhavin.patel@enjayworld.com>
2021-01-18 18:41:08 +0530

    > createMultiField for hadeling multifield value 		=> these takes argument as parent linear layout,key,type,required, typeclass, textinputlayout,editinputtextlayout 		=> New Linearlayout 			=> linear layout 				=> textInputLayout 				=> textinput 				=> imageView 			=> Differencitae the type 				=> setInputType 				=> setFilters 		=> OnClick listener for imagefield 			=> Debugging 			=> getting tag 				=> if main 					then allow the click of imagebutton 						recall the samne function to add phone field with cross image 					else

--- app/src/main/java/com/priyanka/newuat_demo/CreateFeature.java
+++ app/src/main/java/com/priyanka/newuat_demo/CreateFeature.java
@@ -6,11 +6,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.text.InputFilter;
+import android.text.InputType;
 import android.text.util.Linkify;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import com.google.android.material.textfield.TextInputEditText;
@@ -20,6 +29,9 @@ import com.priyanka.newuat_demo.fragment.Details_frag;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+
+import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
+import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
 
 public class CreateFeature extends AppCompatActivity {
 
@@ -72,6 +84,8 @@ public class CreateFeature extends AppCompatActivity {
         array=detail.selectedfileds(module,databasehelper,"edit");
         linearLayout=findViewById(R.id.activty_create_linear_layout);
 
+
+
         //functions calling
         createLayout(array,getApplicationContext(),linearLayout);
 
@@ -80,8 +94,14 @@ public class CreateFeature extends AppCompatActivity {
         // this function is for creating over all Dynamic Layout with
         String key=null;
 
-        TextInputLayout.LayoutParams textinputparams = new TextInputLayout.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.MATCH_PARENT);
+        params = new LinearLayout.LayoutParams(MATCH_PARENT,MATCH_PARENT);
+        params.setMargins(5,5,10,5);
+
+        TextInputLayout.LayoutParams textinputparams = new TextInputLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT);
         textinputparams.setMargins(10,5,10,5);
+        TextInputLayout.LayoutParams edittxtparams = new LinearLayout.LayoutParams(MATCH_PARENT,MATCH_PARENT);
+        edittxtparams.setMargins(10,5,0,5);
+
         for(int i=0;i<details.length();i++){
             try {
                 key=details_frag.getDisplayNames(details.getString(i),"display_label",context);
@@ -90,17 +110,20 @@ public class CreateFeature extends AppCompatActivity {
                     String required=details_frag.getDisplayNames(details.getString(i),"required",context);
 
 //                    Log.e(TAG, "createLayout: this is the type you search for===> "+typr );
-
+                    TextInputLayout textInputLayout;
+                    LinearLayout linearLayout1=new LinearLayout(CreateFeature.this);
+                    linearLayout1.setOrientation(LinearLayout.VERTICAL);
+//                    linearLayout1.setLayoutParams();
                     switch(typr){
                         case "text":
                         case "url":
-                            TextInputLayout textInputLayout= new TextInputLayout(CreateFeature.this);
+                            textInputLayout= new TextInputLayout(CreateFeature.this);
                             textInputLayout.setLayoutParams(textinputparams);
+                            textInputLayout.setPadding(10,5,10,5);
                             TextInputEditText textInputEditText=new TextInputEditText(textInputLayout.getContext());
                             textInputEditText.setTag(R.id.type,typr);
                             textInputEditText.setTag(R.id.key,key);
                             textInputEditText.setTag(R.id.required,required);
-                            textInputLayout.setPadding(10,5,10,5);
                             textInputEditText.setLayoutParams(textinputparams);
                             textInputLayout.setHint(key);
 //                            textInputLayout.setBackground(R.color.material_on_background_emphasis_high_type);
@@ -111,10 +134,12 @@ public class CreateFeature extends AppCompatActivity {
                             String options=details_frag.getDisplayNames(details.getString(i),"options",context);
                             break;
                         case "multi-phone":
-
+                            linearLayout.addView(linearLayout1);
+                            createMultiField(linearLayout1,key,typr,required,InputType.TYPE_CLASS_NUMBER,textinputparams,edittxtparams);
                             break;
                         case "multi-email":
-
+                            linearLayout.addView(linearLayout1);
+                            createMultiField(linearLayout1,key,typr,required,InputType.TYPE_TEXT_VARIATION_WEB_EMAIL_ADDRESS,textinputparams,edittxtparams);
                             break;
                         case "select":
 
@@ -125,6 +150,8 @@ public class CreateFeature extends AppCompatActivity {
                             break;
                         case "multi-address":
 //                        case "multi-tag":
+                            linearLayout.addView(linearLayout1);
+//                            createMultiField(linearLayout1,key,typr,required,InputType.TYPE_TEXT_VARIATION_POSTAL_ADDRESS,textinputparams,edittxtparams);
 
                             break;
                         case "datetime":
@@ -140,15 +167,100 @@ public class CreateFeature extends AppCompatActivity {
         }
     }
 
-//    private void createLayout() {
-//        // this is for creatation of structure
-////        Log.e(TAG, "createLayout: fontend name+a );
-//        Log.e(TAG, "createLayout: "+array);
-//        String key=null;
-//        for (int i=0;i<array.length();i++){
-//            try {
+    private void createMultiField(LinearLayout linearLayout, String key, String typr, String required, int typeClass, LinearLayout.LayoutParams textinputparams, LinearLayout.LayoutParams edittxtparams) {
+        LinearLayout linearLayout1=new LinearLayout(linearLayout.getContext());
+        linearLayout1.setOrientation(LinearLayout.HORIZONTAL);
+        linearLayout1.setLayoutParams(new LinearLayout.LayoutParams(MATCH_PARENT,WRAP_CONTENT));
+//                            Log.e(TAG, "createLayout: linearLayout.getWidth(): "+ linearLayout.getWidth());
+        textInputLayout=new TextInputLayout(linearLayout1.getContext());
+        textinputparams.weight=1;
+        textInputLayout.setLayoutParams(textinputparams);
+        textInputLayout.setPadding(10,5,10,5);
+        TextInputEditText textInputEditText=new TextInputEditText(textInputLayout.getContext());
+        textInputEditText.setTag(R.id.type,typr);
+        textInputEditText.setTag(R.id.key,key);
+        textInputEditText.setTag(R.id.required,required);
+        textInputEditText.setInputType(typeClass);
+        textInputEditText.setFocusable(true);
+        if (key.equals("Phone")){
+            textInputEditText.setFilters(new InputFilter[]{new InputFilter.LengthFilter(10)});
+        }
+        textInputEditText.setLayoutParams(edittxtparams);
+        textInputLayout.setHint(key);
+        textInputLayout.addView(textInputEditText);
+        linearLayout1.addView(textInputLayout);
+
+        params.weight= (float) 10;
+//                            ImageView imageView=new ImageView(linearLayout1.getContext());
+//                            imageView.setImageResource(R.drawable.ic_baseline_more_vert_24);
+//                            imageView.setLayoutParams(params);
+//                            linearLayout1.addView(imageView);
+
+        ImageView imageView1=new ImageView(linearLayout1.getContext());
+        imageView1.setImageResource(R.drawable.ic_baseline_add_24);
+        imageView1.setLayoutParams(params);
+        imageView1.setTag(R.id.tag,"main");
+        linearLayout1.addView(imageView1);
+
+
+        int indexOfMyView = linearLayout.getChildCount();
+        Log.e(TAG, "createMultiField: "+indexOfMyView );
+        if (indexOfMyView>0){
+            imageView1.setImageResource(R.drawable.ic_baseline_clear_24);
+            imageView1.setTag(R.id.tag,null);
+        }
+//        Log.e(TAG, "createMultiField: "+indexOfMyView );
+        linearLayout.addView(linearLayout1,indexOfMyView);
+
+//        imageView1.setOnClickListener(v -> {
+//            Log.e(TAG, "onClick: you clicked plus:" +key);
+//            Log.e(TAG, "createMultiField:this is your resource tag===> "+imageView1.getTag(R.id.resource) );
+////            if (imageView1.getTag(R.id.resource)!=null){
+////                Log.e(TAG, "onClick: removing func called" +linearLayout.getChildAt(R.id.resource));
+////                linearLayout.removeView(linearLayout.getChildAt(R.id.resource));
+////            }else{
 //
+//            Log.e(TAG, "createMultiField: indexOfMyView called:"+linearLayout.getChildCount()+" || "+indexOfMyView);
+//            for (int i=0;i<linearLayout.getChildCount();i++){
+//                Log.e(TAG, "createMultiField: for loop called:"+linearLayout.getChildCount()+" || "+indexOfMyView );
+//                if (linearLayout.getChildAt(i) instanceof LinearLayout){
+//                    Log.e(TAG, "createMultiField: th" );
+//                    int temp=i;
+//                    Log.e(TAG, "createMultiField: this is temp field==>"+temp);
+//                    if (linearLayout.getChildCount()==indexOfMyView+1){
+//                        if (!textInputEditText.getText().toString().isEmpty()) {
+//                            Log.e(TAG, "onClick: this is your edit text text+++>> "+textInputEditText.getText() );
+//                            createMultiField(linearLayout, key, typr, required, typeClass, textinputparams, edittxtparams);
+//                        }else {
+//                            textInputEditText.setError("Empty");
+//                            textInputEditText.setFocusable(true);
+//                        }
+//                    }
+//                }
 //            }
-//        }
-//    }
+//        });
+
+    imageView1.setOnClickListener(v -> {
+        try {
+            if (imageView1.getTag(R.id.tag).equals("main")) {
+                Log.e(TAG, "createMultiField: you clicked first one===:");
+                Log.e(TAG, "createMultiField:these are my child's linear Layout==> "+linearLayout.getChildCount());
+                if (!textInputEditText.getText().toString().isEmpty()) {
+                    Log.e(TAG, "onClick: this is your edit text text+++>> " + textInputEditText.getText());
+                    createMultiField(linearLayout, key, typr, required, typeClass, textinputparams, edittxtparams);
+                } else {
+                    textInputEditText.setError("Empty");
+                    textInputEditText.setFocusable(true);
+                }
+            } else {
+                //here write the logic for removing view
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    });
+    }
+
+
 }
